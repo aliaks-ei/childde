3c090d8446a1cdf3d527ceff5f658b819c7bc7b6
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-04-09 19:05:58 +0300

    Make swipers independent

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,24 @@
 const swiperParams = {
-    slidesPerView : 2.8,
+    slidesPerView : '2.8',
     spaceBetween  : 20,
     freeMode      : true,
-    grabCursor    : true,
-    navigation    : {
-        nextEl : '.swiper-button-next',
-        prevEl : '.swiper-button-prev',
-    }
+    grabCursor    : true
 };
 
-const personages      = new Swiper('#personagesSwiper .swiper-container', swiperParams);
-const questionsSlider = new Swiper('#questionsSlider .swiper-container', swiperParams);
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperEls = document.getElementsByClassName('base-swiper');
+
+    for (let i = 0; i < swiperEls.length; i++) {
+        swiperEls[i].querySelector('.swiper-container').classList.add(`swiper-${ i + 1 }`);
+        swiperEls[i].querySelector('.swiper-button-prev').classList.add(`btn-prev-${ i + 1 }`);
+        swiperEls[i].querySelector('.swiper-button-next').classList.add(`btn-next-${ i + 1 }`);
+
+        const swiper = new Swiper(`.swiper-${ i + 1 }`, {
+            ...swiperParams,
+            navigation: {
+                nextEl : `.btn-next-${ i + 1 }`,
+                prevEl : `.btn-prev-${ i + 1 }`,
+            }
+        })
+    }
+});
