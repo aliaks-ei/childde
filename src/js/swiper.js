d6b7cb1174be03adc56f53b8c65b4cfadb68e041
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-04-09 15:34:56 +0300

    Almost finish with swiper component

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,4 @@
-const personages = new Swiper('#personagesSwiper .swiper-container', {
+const swiperParams = {
     slidesPerView : 2.8,
     spaceBetween  : 20,
     freeMode      : true,
@@ -7,4 +7,7 @@ const personages = new Swiper('#personagesSwiper .swiper-container', {
         nextEl : '.swiper-button-next',
         prevEl : '.swiper-button-prev',
     }
-});
+};
+
+const personages      = new Swiper('#personagesSwiper .swiper-container', swiperParams);
+const questionsSlider = new Swiper('#questionsSlider .swiper-container', swiperParams);
