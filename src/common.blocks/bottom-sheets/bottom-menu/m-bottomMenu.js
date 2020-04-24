a5d1d223d7aeeac66f66ebb9bd5476134abb1979
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-04-24 11:36:02 +0300

    Update mobile version

--- src/common.blocks/bottom-sheets/bottom-menu/m-bottomMenu.js
+++ src/common.blocks/bottom-sheets/bottom-menu/m-bottomMenu.js
@@ -2,13 +2,15 @@ const bottomMenu = document.getElementById('bottomMenu');
 const input      = bottomMenu.querySelector('input');
 
 input.addEventListener('focus', () => {
-    const topPosition = window.scrollY + (window.innerHeight - bottomSheet.offsetHeight) + 95 + 'px';
+    const topPosition = window.scrollY + (window.innerHeight - bottomSheet.offsetHeight) + 100 + 'px';
 
     bottomSheet.classList.add('bs--keyboard-active');
     bottomSheet.style.setProperty('top', topPosition, 'important');
 });
 
 input.addEventListener('blur', () => {
-    bottomSheet.classList.remove('bs--keyboard-active');
-    bottomSheet.style.setProperty('top', 'auto', 'important');
+    setTimeout(() => {
+        bottomSheet.style.setProperty('top', 'auto', 'important');
+        bottomSheet.classList.remove('bs--keyboard-active');
+    }, 300);
 });
