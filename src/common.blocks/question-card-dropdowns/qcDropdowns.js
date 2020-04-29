bffc7b10c93c04dbb4840793cf58dbf279605b08
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-04-29 16:16:30 +0300

    Merge qc dropdowns

--- src/common.blocks/question-card-dropdowns/qcDropdowns.js
+++ src/common.blocks/question-card-dropdowns/qcDropdowns.js
@@ -38,4 +38,15 @@ window.addEventListener('click', event => {
         qBottomDropdownEl = document.getElementById('qBottomDropdown');
         generateQBottomDropdownEl(x, y)
     }
+
+    if (event.target.dataset && event.target.dataset.target === 'qcTopMenu') {
+        document.getElementById('qcTopMenu').style.display = 'block';
+
+        showBottomSheet();
+    }
+    else if (event.target.dataset && event.target.dataset.target === 'qcBottomMenu') {
+        document.getElementById('qcBottomMenu').style.display = 'block';
+
+        showBottomSheet();
+    }
 });
