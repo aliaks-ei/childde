fc480273ba35f539f9b3106ca42af8b76a684621
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-05-14 12:42:42 +0300

    Include footer to 404 page

--- src/common.blocks/search-bar/searchBar.js
+++ src/common.blocks/search-bar/searchBar.js
@@ -7,11 +7,12 @@ for (const activator of searchBarActivators) {
 
         if (searchBarEl.classList.contains('search-bar--visible')) {
             searchBarEl.classList.remove('search-bar--visible');
-            bottomNav.style.bottom = '24px';
+
+            if (bottomNav) bottomNav.style.bottom = '24px';
         }
         else {
             searchBarEl.classList.add('search-bar--visible');
-            bottomNav.style.bottom = '-96px';
+            if (bottomNav) bottomNav.style.bottom = '-96px';
 
             setTimeout(() => searchBarEl.querySelector('.search-bar__input').focus(), 100);
         }
