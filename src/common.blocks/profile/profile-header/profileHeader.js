5f7d3c17267b72ce0fe03bea2bb1754b0fc45f66
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-05-26 15:48:21 +0300

    Implement 'UpdateProfileMenu' component

---
+++ src/common.blocks/profile/profile-header/profileHeader.js
@@ -0,0 +1,12 @@
+(function () {
+    const updateProfileMenuActivator = document.querySelector('[data-target="updateProfileMenu"]');
+    const updateProfileMenu          = document.getElementById('updateProfileMenu')
+
+    if (updateProfileMenuActivator) {
+        window.addEventListener('click', function (event) {
+            const showUpdateProfileMenu = updateProfileMenuActivator.contains(event.target) || updateProfileMenu.contains(event.target);
+            
+            updateProfileMenu.style.display = showUpdateProfileMenu ? 'block' : 'none';
+        });
+    }
+})();
