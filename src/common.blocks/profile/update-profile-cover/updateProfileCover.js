338fa398aae55bcf172e357468430b454af5a9a5
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-05-27 13:31:24 +0300

    Implement 'updateProfileCoverModal' component

---
+++ src/common.blocks/profile/update-profile-cover/updateProfileCover.js
@@ -0,0 +1,25 @@
+(function () {
+    const updateProfileCoverActivator = document.querySelector('[data-target="profileUpdateCover"]');
+    const updateProfileCoverCloseBtn  = document.querySelector('[data-target-close="profileUpdateCover"]');
+    const updateProfileCoverMenu      = document.getElementById('profileUpdateCover');
+
+    function handleUpdateProfileCoverMenuClick(event) {
+        if (updateProfileCoverCloseBtn.contains(event.target) || !updateProfileCoverMenu.contains(event.target)) {
+            updateProfileCoverMenu.style.display = 'none';
+
+            window.removeEventListener('click', handleUpdateProfileCoverMenuClick);
+        }
+    }
+
+    updateProfileCoverActivator && updateProfileCoverActivator.addEventListener('click', function (event) {
+        updateProfileCoverMenu.style.display = 'block';
+
+        event.stopPropagation();
+
+        if (updateProfileMenu) {
+            updateProfileMenu.style.display = 'none';
+        }
+
+        window.addEventListener('click', handleUpdateProfileCoverMenuClick);
+    });
+})();
