0afd1e6a4143b1ca7898d8a42314381ce3802a52
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-05-27 17:46:55 +0300

    Implement otherProfileMenu

---
+++ src/common.blocks/profile/other-profile-menu/otherProfileMenu.js
@@ -0,0 +1,14 @@
+(function () {
+    const otherProfileMenuActivator = document.querySelector('[data-target="otherProfileMenu"]');
+    const otherProfileMenu          = document.getElementById('otherProfileMenu');
+
+    window.addEventListener('click', function (event) {
+        if (!(otherProfileMenuActivator || otherProfileMenu)) return;
+
+        otherProfileMenu.style.display = 'none';
+
+        if (otherProfileMenuActivator.contains(event.target) || otherProfileMenu.contains(event.target)) {
+            otherProfileMenu.style.display = 'block';
+        }
+    });
+})();
