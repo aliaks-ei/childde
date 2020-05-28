a10bf76f197e00cf03a61ae90de1dc8de263aad8
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-05-28 14:26:50 +0300

    Implement themes rating

--- src/common.blocks/profile/profile-tabs/profileTabs.js
+++ src/common.blocks/profile/profile-tabs/profileTabs.js
@@ -1,22 +1,25 @@
 (function () {
     const profileTabsWrapper = document.querySelector('.profile-tabs-wrapper');
     const profileTabLinks    = document.querySelectorAll('.profile-tabs__link');
-    const tabsContent        = profileTabsWrapper.nextElementSibling.children;
 
-    profileTabsWrapper && profileTabsWrapper.addEventListener('click', function (event) {
-        const currentProfileTabLink = event.target.closest('.profile-tabs__link');
+    if (profileTabsWrapper) {
+        const tabsContent = profileTabsWrapper.nextElementSibling.children;
 
-        if (currentProfileTabLink) {
-            const hash              = new URL(currentProfileTabLink.href).hash.substr(1);
-            const currentTabContent = document.getElementById(hash);
+        profileTabsWrapper.addEventListener('click', function (event) {
+            const currentProfileTabLink = event.target.closest('.profile-tabs__link');
 
-            [...profileTabLinks].forEach(profileTab => profileTab.classList.remove('profile-tabs__link--active'));
-            [...tabsContent].forEach(tabContent => tabContent.style.display = 'none');
+            if (currentProfileTabLink) {
+                const hash              = new URL(currentProfileTabLink.href).hash.substr(1);
+                const currentTabContent = document.getElementById(hash);
 
-            currentProfileTabLink.classList.add('profile-tabs__link--active');
-            currentTabContent.style.display = 'block';
+                [...profileTabLinks].forEach(profileTab => profileTab.classList.remove('profile-tabs__link--active'));
+                [...tabsContent].forEach(tabContent => tabContent.style.display = 'none');
 
-            event.preventDefault();
-        }
-    });
+                currentProfileTabLink.classList.add('profile-tabs__link--active');
+                currentTabContent.style.display = 'block';
+
+                event.preventDefault();
+            }
+        });
+    }
 })();
