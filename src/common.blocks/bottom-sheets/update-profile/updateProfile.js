f5c932c35ee8ebc7e6e95bd8f49912e279fcc800
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-05-28 18:35:35 +0300

    Implement upload profile cover bottom sheet

---
+++ src/common.blocks/bottom-sheets/update-profile/updateProfile.js
@@ -0,0 +1,30 @@
+(function () {
+    const bsUpdateProfile           = document.getElementById('bsUpdateProfile');
+    const bsUpdateProfileActivators = document.querySelectorAll('[data-target="bsUpdateProfile"]');
+
+    function handleBsUpdateProfileClick(event) {
+        const currentMenuItem = event.target.closest('.update-profile-menu__item-wrapper');
+
+        if (currentMenuItem) {
+            hideBottomSheet();
+
+            bottomSheet.addEventListener('transitionend', function () {
+                if (!currentMenuItem.dataset.target && bsCoverUpload) {
+                    bsCoverUpload.style.display = 'block';
+    
+                    showBottomSheet();
+                }
+            }, { once: true });
+        }
+    }
+
+    for (const activator of bsUpdateProfileActivators) {
+        activator.addEventListener('click', () => {
+            bsUpdateProfile.style.display = 'block';
+
+            bsUpdateProfile.addEventListener('click', handleBsUpdateProfileClick);
+
+            showBottomSheet();
+        });
+    }
+})();
