23ca2c5c60abb836283192a53e1af32233688e3f
Alexey Mozheyko <aleksmozheiko94@gmail.com>
2020-06-03 16:17:44 +0300

    Update footer

--- src/common.blocks/footer/base/baseFooter.js
+++
@@ -1,6 +0,0 @@
-const scrollToTopBtn = document.getElementById('scrollToTop');
-
-scrollToTopBtn && scrollToTopBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
